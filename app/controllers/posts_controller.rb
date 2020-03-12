3eff8e434e2588c3e7ba131a83d7b531fec3ed6a
makoto1026 <trsmmkt@gmail.com>
2020-03-12 22:44:34 +0900

    showView-create-commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,11 @@ class PostsController < ApplicationController
 
   def create
     Post.create(post_params)
+    redirect_to root_path
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -18,6 +20,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(15)
   end
 
   # def search
@@ -29,6 +32,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :body, :image, :material).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
   def move_to_index
