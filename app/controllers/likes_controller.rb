1f17e8c335bff1bddef10db9cde100a63315a127
makoto1026 <trsmmkt@gmail.com>
2020-03-13 20:52:52 +0900

    mainPage-jQuery-commit

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,18 @@
+class LikesController < ApplicationController
+  def create
+    @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+    @likes = Like.where(post_id: params[:post_id])
+    get_post
+  end
+
+  def destroy    
+    @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    @like.destroy
+    @likes = Like.where(post_id: params[:post_id])
+    get_post
+  end
+
+  def get_post
+    @post = Post.find(params[:post_id])
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,19 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_many   :comments
+  has_many :likes, dependent: :destroy
+
+  validates :title, :body, :material, presence: :true
+
+  mount_uploader :image, ImagesUploader
 
   def self.search(search)
     return Post.all unless search    
     Post.where('title LIKE(?)', "%#{search}%")
   end
 
-  validates :title, :body, :material, presence: :true
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+   end
 
-  mount_uploader :image, ImagesUploader
 end
