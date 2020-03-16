1d6ff8acaee76e3665f14e7455afa354fd1f2c3c
makoto1026 <trsmmkt@gmail.com>
2020-03-16 21:56:27 +0900

    add-seeds-ancestry-commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200316120828) do
+ActiveRecord::Schema.define(version: 20200316123844) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "ancestry"
+    t.string   "name"
     t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
   end
 
