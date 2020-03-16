76902aadb686e449f727bcacf4583594ac38cdbf
makoto1026 <trsmmkt@gmail.com>
2020-03-16 21:11:52 +0900

    ancestry-model-controller-table-create-commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   has_many   :comments
   has_many :likes
   has_many :liked_users, through: :likes, source: :user
+  belongs_to :category
 
   validates :title, :body, :material, presence: :true
 
