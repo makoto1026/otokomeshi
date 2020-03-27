9af576df8064b2da15fa1eaa0158863030db128d
makoto1026 <trsmmkt@gmail.com>
2020-03-27 16:53:59 +0900

    deploy3-commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  server '18.178.138.85', user: 'ec2-user', roles: %w{app db web}
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
